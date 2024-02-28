40733c2aafbc43a36fb008dbfb998cd906a460ca
nataliadolina <45314570+nataliadolina@users.noreply.github.com>
2024-02-28 17:29:42 +0300

    added db

--- Mocks/MockCars.cs
+++ Mocks/MockCars.cs
@@ -12,7 +12,8 @@ namespace Shop.Mocks
             {
                 return new List<Car>
                 {
-                    new Car {Name="Tesla", ShortDesc= "", LongDesc="", Image = "", Price = 45000, IsFavourite = true, Available = true, Category = _carsCategory.AllCategories.First() }
+                    new Car {Name="Tesla", ShortDesc= "Eco-friendly electric vehicles with exemplary technical characteristics. These cars travel up to 500 kilometers on a single charge and show good dynamics.", LongDesc="The Model S Ludicrous accelerates from 0 to 100 km/h in 3 seconds. Three models of electric vehicles are sold under the Tesla brand: S 3 X Y History. Elon Musk is the founder of the company and one of the creators of the PayPal payment system.", Image = "/img/tesla1.jpg", Price = 45000, IsFavourite = true, Available = true, Category = _carsCategory.AllCategories.First() },
+                    new Car {Name="Nissan", ShortDesc="A pioneer in electric vehicles, Nissan has not only made them affordable but also more practical by investing in charging infrastructure and energy management.", LongDesc="Nissan is advancing initiatives that utilize mobility to realize a sustainable society. We'll continue to provide services that connect vehicles with people and society.", Image="/img/nissan.jpg", Price=30000, IsFavourite = true, Available = true, Category = _carsCategory.AllCategories.First() }
                 };
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
+using Shop.Data;
 using Shop.Interfaces;
 using Shop.Mocks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer;
+using Shop.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json",
+        optional: false,
+        reloadOnChange: true);
 
+string connection = builder.Configuration["DefaultConnection"];
+
+builder.Services.AddDbContext<AppDBContent>(options => options.UseSqlServer(connection));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IAllCars, MockCars>();
-builder.Services.AddTransient<ICarsCategory, MockCategory>();
+builder.Services.AddTransient<IAllCars, CarRepository>();
+builder.Services.AddTransient<ICarsCategory, CategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,5 +37,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+using (var scope = app.Services.CreateScope())
+{
+    AppDBContent content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
+    DBObjects.Initial(content);
+}
 app.Run();
